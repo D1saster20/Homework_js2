6560b4920e2fc57f2bc74e7c59e91587b2657ad5
D1saster20 <yuriizhukevych@gmail.com>
2022-12-13 19:01:03 +0200

    1

---
+++ main.js
@@ -0,0 +1,14 @@
+let arr = [123, 6, 'string', 732, true, false, 54, 'change', 34, 78];
+console.log(arr)
+let books = {
+    title: 'Potter',
+    pageCount: 600,
+    genre: 'Fantasy',
+    authors:{name:'rouling',age:63}
+}
+let users = {
+    name:' artur',
+    username: 'disaster20',
+    pasword:1234
+}
+console.log(users.pasword)
